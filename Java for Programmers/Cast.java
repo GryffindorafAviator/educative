e7b7de7d3af665e2cd8ac940d5c51cd6c73429d9
Ruiyue Wang <phynew@hotmail.com>
2021-10-27 15:03:54 -0400

    Create Cast.java

---
+++ Java for Programmers/Cast.java
@@ -0,0 +1,10 @@
+class Cast {
+  public static void main(String args[]) {
+    int numerator = 18;
+    int denominator = 5;
+    double result = numerator / denominator;
+
+    System.out.println(result);
+    
+  }
+}
